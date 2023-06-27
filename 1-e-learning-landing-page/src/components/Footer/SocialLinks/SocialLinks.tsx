72e0896a58ac40c8abd605e59cb7037b7f159a6d
malunaridev <malunaridev@gmail.com>
2023-06-27 16:36:06 -0300

    ♻️ refactor: replace image for testing

--- 1-e-learning-landing-page/src/components/Footer/SocialLinks/SocialLinks.tsx
+++ 1-e-learning-landing-page/src/components/Footer/SocialLinks/SocialLinks.tsx
@@ -1,16 +1,16 @@
 import './SocialLinks.css';
 import instagram from "../../../assets/Instagram.png"
 import facebook from "../../../assets/Facebook.png"
-import facebook2 from "@/assets/Facebook.png"
+import twitter from "../../../assets/Twitter.png"
+import whatsapp from "../../../assets/Whatsapp.png"
 
 export const SocialLinks = () => {
-    console.log(facebook2)
     return (
         <div>
         <a href="https://www.instagram.com" target='blank'><img className="SocialLink" src={instagram}/></a>
-        <a href="https://www.facebook.com" target='blank'><img className="SocialLink" src={facebook2}/></a>
-        <a href="https://www.twitter.com" target='blank'><img className="SocialLink" src='./src/assets/Twitter.png'/></a>
-        <a href="https://www.whatsApp.com" target='blank'><img className="SocialLink" src='./src/assets/WhatsApp.png'/></a>
+        <a href="https://www.facebook.com" target='blank'><img className="SocialLink" src={facebook}/></a>
+        <a href="https://www.twitter.com" target='blank'><img className="SocialLink" src={twitter}/></a>
+        <a href="https://www.whatsApp.com" target='blank'><img className="SocialLink" src={whatsapp}/></a>
         </div>
     )
 }
