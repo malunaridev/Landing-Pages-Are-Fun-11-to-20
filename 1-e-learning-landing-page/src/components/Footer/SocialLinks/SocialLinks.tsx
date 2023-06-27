a5eefec3ada507565dbfd706222a9b0bbf695a08
malunaridev <malunaridev@gmail.com>
2023-06-27 16:20:02 -0300

    ♻️ refactor: replace image for testing

--- 1-e-learning-landing-page/src/components/Footer/SocialLinks/SocialLinks.tsx
+++ 1-e-learning-landing-page/src/components/Footer/SocialLinks/SocialLinks.tsx
@@ -1,9 +1,10 @@
 import './SocialLinks.css';
+import instagram from "../../../assets/Instagram.png"
 
 export const SocialLinks = () => {
     return (
         <div>
-        <a href="https://www.instagram.com" target='blank'><img className="SocialLink" src="./Instagram.png"/></a>
+        <a href="https://www.instagram.com" target='blank'><img className="SocialLink" src={instagram}/></a>
         <a href="https://www.facebook.com" target='blank'><img className="SocialLink" src='./src/assets/Facebook.png'/></a>
         <a href="https://www.twitter.com" target='blank'><img className="SocialLink" src='./src/assets/Twitter.png'/></a>
         <a href="https://www.whatsApp.com" target='blank'><img className="SocialLink" src='./src/assets/WhatsApp.png'/></a>
