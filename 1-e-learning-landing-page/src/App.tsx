5d9d33f6bd4f2c096e46ad3793a1a7e5dd18c90f
malunaridev <malunaridev@gmail.com>
2023-06-29 22:38:16 -0300

    🩹 fix: fix wrong image path

--- 1-e-learning-landing-page/src/App.tsx
+++ 1-e-learning-landing-page/src/App.tsx
@@ -3,6 +3,8 @@ import { PageContent } from "./components/PageContent/PageContent"
 import { TextContent } from "./components/PageContent/TextContent/TextContent"
 import { Footer } from "./components/Footer/Footer"
 import { SocialLinks } from "./components/Footer/SocialLinks/SocialLinks"
+import ChatIcon from "./assets/Chat.png"
+import ChildPic from "./assets/child.png"
 
 function App() {
 
@@ -11,11 +13,11 @@ function App() {
       <Header/>
       <PageContent>
         <TextContent/>
-        <img className="carouselPic" src="./assets/child.png"/>
+        <img className="carouselPic" src={ChildPic}/>
       </PageContent>
     <Footer>
       <SocialLinks/>
-      <img className="chatIcon" src="./assets/Chat.png"/>
+      <img className="chatIcon" src={ChatIcon}/>
     </Footer>
     </div>
   )
